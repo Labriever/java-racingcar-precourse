8f1d44f4127b21171eed2c6843c1b9099af79520
Labriever <liveinmars2018@gmail.com>
2024-07-11 11:52:34 +0900

    전진하는 자동차 출력 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -23,7 +23,15 @@ public class Car {
     }
     
 //    각 자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
-    
+    public String print() {
+    	StringBuilder printing = new StringBuilder();
+    	printing.append(name + " : ");
+    	for(int i=0;i<position;i++) {
+    		printing.append("-");
+    	}
+    	String result = printing.toString();
+    	return result;
+    }
     
 //    자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
 
