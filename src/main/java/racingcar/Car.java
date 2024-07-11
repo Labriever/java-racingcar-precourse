d61a32c27041c64e838b77296b7878fedab51fc6
Labriever <liveinmars2018@gmail.com>
2024-07-11 11:07:13 +0900

    [Labriever#1] 전진 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -7,13 +9,16 @@ public class Car {
     public Car(String name) {
         this.name = name;
     }
-
+    
     // 추가 기능 구현
-
+    
 //    주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다.
 //    전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
     public void move() {
-    	
+    	int num = Randoms.pickNumberInRange(0, 9);
+    	if (num >= 4) {
+    		position++;
+    	}
     }
     
 //    각 자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
