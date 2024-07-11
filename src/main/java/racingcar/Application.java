a0d2a8d7afaa576670f79e862e2fa334c494ff68
Labriever <liveinmars2018@gmail.com>
2024-07-11 12:26:21 +0900

    error 타입 설정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,23 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
-        System.out.println("?");
+    	Scanner sc = new Scanner(System.in);
 //      자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
+    	String names = sc.next();
+    	String[] nameArr = names.split(",");
+    	
+    	
 //      사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
-        Car car = new Car("position");
+//      자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
         
+//      우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.
+
+//      사용자가 잘못된 값을 입력할 경우 IllegalArgumentException를 발생시키고, "[ERROR]"로 시작하는 에러 메시지를 출력 후 그 부분부터 입력을 다시 받는다.
+    	
     }
 }
