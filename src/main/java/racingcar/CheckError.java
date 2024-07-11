53a4849f5f31736da29ee4881177b5b88bd2effd
Labriever <liveinmars2018@gmail.com>
2024-07-11 14:53:27 +0900

    feat: 경기 횟수 에러 메소드 작성

--- src/main/java/racingcar/CheckError.java
+++ src/main/java/racingcar/CheckError.java
@@ -17,6 +17,12 @@ public class CheckError {
 
 		}
 		
+		public void isCount(int count) {
+			if(count > 5 || count < 1) {
+				throw new IllegalArgumentException(printError(3));
+			}
+		}
+		
 //      사용자가 잘못된 값을 입력할 경우 IllegalArgumentException를 발생시키고, "[ERROR]"로 시작하는 에러 메시지를 출력 후 그 부분부터 입력을 다시 받는다.
 		public static String printError(int errorType) {
 			String error = "[ERROR]";
